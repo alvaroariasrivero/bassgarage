5de30550c709a5e5a45bcad03fb0567f0293689b
Álvaro Arias Rivero <alvaroariasrivero92@gmail.com>
2022-06-09 11:59:55 +0200

    added selectPic function

---
+++ scripts/index.js
@@ -0,0 +1,10 @@
+console.log('Hello world')
+
+const littlePics = document.getElementsByClassName("little");
+const bigPic = document.getElementById('bigPic');
+
+console.log(bigPic)
+
+const selectPic = () => {
+    console.log(littlePics.src);
+};
