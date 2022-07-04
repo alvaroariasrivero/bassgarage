5b86e775b073d7e70254d1b1b92ffc2421527723
Álvaro Arias Rivero <alvaroariasrivero92@gmail.com>
2022-07-04 23:37:13 +0200

    disabled page div

--- scripts/index.js
+++ scripts/index.js
@@ -2,6 +2,7 @@ const littlePics = document.getElementsByClassName("little");
 const price = document.getElementById('price');
 const quantity = document.getElementById('quantity');
 const cart = document.getElementById('cart');
+const disabled = document.getElementById('disabled');
 
 const selectPic = () => {
     for(let i = 0; i < littlePics.length; i++){
@@ -18,9 +19,21 @@ const totalPrice = () => {
 
 const payPurchase = (event) => {
     event.preventDefault();
-    cart.innerText = `El precio total son ${quantity.value * 2.388}€, muchas gracias por su compra`;
+    cart.innerHTML = `<h4>El precio total son ${quantity.value * 2.388}€, muchas gracias por su compra</h4>
+                    <div class="purchase">
+                        <button class="orange-button return">Continuar comprando</button>
+                        <button class="orange-button return">Pagar la compra</button>
+                    </div>
+                    <h4>¡Vuelva pronto!</h4>`;
     // cart.style.position = 'absolute'
-    cart.style.display = 'block'
+    disabled.style.display = 'block';
+    cart.style.display = 'block';
+    let scrollTop = window.pageYOffset || document.documentElement.scrollTop;
+    let scrollLeft = window.pageXOffset || document.documentElement.scrollLeft;
+    // if any scroll is attempted, set this to the previous value
+    window.onscroll = function() {
+        window.scrollTo(scrollLeft, scrollTop);
+    };
 };
 
 selectPic();
