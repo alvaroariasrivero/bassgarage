0aee439eca435f759cd59782c9a55053ac183c5a
Álvaro Arias Rivero <alvaroariasrivero92@gmail.com>
2022-07-06 19:58:36 +0200

    script done

--- scripts/index.js
+++ scripts/index.js
@@ -3,6 +3,9 @@ const price = document.getElementById('price');
 const quantity = document.getElementById('quantity');
 const cart = document.getElementById('cart');
 const disabled = document.getElementById('disabled');
+const card = document.getElementById('card');
+const title = document.getElementById('product-title');
+price.innerText = quantity.value * 2.388;
 
 const selectPic = () => {
     for(let i = 0; i < littlePics.length; i++){
@@ -14,7 +17,7 @@ const selectPic = () => {
 };
 
 const totalPrice = () => {
-    price.innerText = `Total ${quantity.value * 2.388}€`;
+    price.innerText = `${quantity.value * 2.388}`;
 };
 
 const payPurchase = (event) => {
@@ -23,7 +26,7 @@ const payPurchase = (event) => {
         cart.innerHTML = `<h4>El precio total son ${quantity.value * 2.388}€, muchas gracias por su compra</h4>
                     <div class="purchase">
                         <button class="orange-button return" onclick="continueBuying()">Continuar comprando</button>
-                        <button class="orange-button return" onclick="location.href = '#';">Pagar la compra</button>
+                        <button class="orange-button return" onclick="goToCart()">Pagar la compra</button>
                     </div>
                     <h4>¡Vuelva pronto!</h4>`;
         disabled.style.display = 'block';
@@ -42,6 +45,32 @@ const continueBuying = () => {
     cart.style.display = 'none';
     //enable scroll
     window.onscroll = function() {};
+}
+
+const goToCart = () => {
+    disabled.style.display = 'none';
+    cart.style.display = 'none';
+    //enable scroll
+    window.onscroll = function() {};
+    window.scrollTo(0, 0);
+    card.innerHTML = `<div class="bill-card">
+                        <div class="bill">
+                            <div class="flex-item">
+                                <img src="${littlePics[0].src}" alt="image1" class="little">
+                            </div>
+                            <div class="flex-item">
+                                <p><b>Nombre:</b> ${title.innerHTML}</p>
+                            </div>
+                            <div class="flex-item">
+                                <p><b>Cantidad:</b> ${quantity.value}</p>
+                            </div>
+                        </div>
+                        <p class="total-price"><b>Precio total:</b> ${price.innerText}€</p>
+                    </div>`;
+    littlePics[0].style.cursor = 'default';
+    littlePics[0].style.border = 'none';
+    card.style.flexDirection = 'row';
+
 }
 
 selectPic();
